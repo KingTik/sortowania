f4e0702f076a4838b6351ce0de399098bb10809f
twesolowski <twesolowski@enterso.pl>
2017-05-11 12:01:32 +0200

    test dla bubble

--- bubble.py
+++ bubble.py
@@ -1,5 +1,6 @@
 import numpy
-
+import math
+import copy
 def bubbleSort(tablica):
     """ sortowanie bombelkowe """
     for i in range(0, len(tablica)-1):
@@ -14,9 +15,18 @@ def bubbleSort(tablica):
 
 
 
+for i in range(0, 1000):
+    
+    tab = [int(99*numpy.random.random()) for i in xrange(int(999*numpy.random.random()))]
+    prev = copy.deepcopy(tab)
+    szel = bubbleSort(tab)
+    prev.sort()
 
-tab = [int(99*numpy.random.random()) for i in xrange(25)]
 
-print tab
-print bubbleSort(tab)
-
+    if prev == szel:
+        pass
+    else:
+        print 'rozne'
+        print prev
+        print szel
+        break
